60160510ba0cb668f4b6462803af46c222b8f6e3
Karanveer Singh <karansingh98123@gmail.com>
2019-06-05 10:47:35 -0400

    chnaged name of file from zip-code.js to zipCode.js

---
+++ zip-code-search/src/components/zipCode.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import "./zipCode.css";
+
+class ZipCode extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            zipCode: 0
+        };
+    }
+
+    handleOnChange = (event) => {
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onChange={this.handleOnChange}>
+                    Enter a Zip Code <br/>
+                    <input type="number" name="zipCode"/>
+                </form>
+            </div>
+        );
+    }
+
+};
+
+export default ZipCode;
