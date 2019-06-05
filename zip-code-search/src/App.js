4d21b844ea8b32613b293332726adb216f2edc8d
Karanveer Singh <karansingh98123@gmail.com>
2019-06-05 10:49:24 -0400

    changed file path for import ZipCode to match new zipCode.js file

--- zip-code-search/src/App.js
+++ zip-code-search/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ZipCode from './components/zip-code.js';
+import ZipCode from './components/zipCode.js';
 
 function App() {
   return (
